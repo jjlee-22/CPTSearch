52e2a9399dda5ebdd8e49fe82ea0560bd2811850
Jonathan <jjlee015@gmail.com>
2019-09-16 15:56:14 -0400

    Initial Commit

---
+++ CPTSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPTSearch
+//
+//  Created by Jhoonil on 9/16/19.
+//  Copyright © 2019 Cleveland Clinic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
