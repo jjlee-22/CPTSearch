1cb061decec7f09b7c08b893ff886e43bc5bc41c
Jonathan <jjlee015@gmail.com>
2019-09-18 16:38:33 -0400

    add two buttons for physician and specialist

--- CPTSearch/ViewController.swift
+++ CPTSearch/ViewController.swift
@@ -2,7 +2,9 @@
 //  ViewController.swift
 //  CPTSearch
 //
-//  Created by Jhoonil on 9/16/19.
+//  Description: CPTSearch is an iOS ordering app that searches for the correct imaging exam and processes the orders for the doctors
+//
+//  Created by Jonathan Lee on 9/16/19.
 //  Copyright © 2019 Cleveland Clinic. All rights reserved.
 //
 
@@ -10,6 +12,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // This method only runs after the view loads
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
