40f39b33306a870f83a9e7a5c661e58847278c6f
Truc Tran <trantruc44@gmail.com>
2019-12-04 16:24:08 -0500

    Functionality for Add New Order Button

--- CPTSearch/AddOrder.swift
+++ CPTSearch/AddOrder.swift
@@ -2,8 +2,27 @@
 //  AddOrder.swift
 //  CPTSearch
 //
-//  Created by Truc Tran  on 12/4/19.
+//  Created by Truc Tran  on 12/2/19.
 //  Copyright © 2019 Cleveland Clinic. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AddOrder: UIViewController {
+    
+
+    @IBOutlet weak var CPTCodeTextBox: UITextField!
+    
+    @IBOutlet weak var CPTShortTextBox: UITextField!
+    
+    @IBOutlet weak var CPTLongTextBox: UITextField!
+    
+    @IBAction func AddOrderButton(_ sender: Any) {
+        CPTCodeData.append(CPTCodeTextBox.text!)
+        shortData.append(CPTShortTextBox.text!)
+        longData.append(CPTLongTextBox.text!)
+         CPTDictionary[CPTCodeData[deleteCounter]] = [shortData[deleteCounter], longData[deleteCounter]]
+        deleteCounter+=1
+    }
+}
+
