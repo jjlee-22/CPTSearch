d6c46f9ed9125e1021528f6a45c183e4055a5087
Lindsey <lherman21@jcu.edu>
2019-12-04 17:08:18 -0500

    Has Truc's AddOrder

--- CPTSearch/AddOrder.swift
+++ CPTSearch/AddOrder.swift
@@ -2,13 +2,15 @@
 //  AddOrder.swift
 //  CPTSearch
 //
-//  Created by Truc Tran  on 12/2/19.
+//  Created by Student on 12/4/19.
 //  Copyright © 2019 Cleveland Clinic. All rights reserved.
 //
 
 import UIKit
 
 class AddOrder: UIViewController {
+
+    
     
     @IBOutlet weak var CPTCodeTextBox: UITextField!
     
@@ -17,12 +19,14 @@ class AddOrder: UIViewController {
     @IBOutlet weak var CPTLongTextBox: UITextField!
     
 
+    
+    
+
     @IBAction func AddOrderButton(_ sender: Any) {
-        CPTCodeData.append(CPTCodeTextBox.text!)
+    CPTCodeData.append(CPTCodeTextBox.text!)
         shortData.append(CPTShortTextBox.text!)
         longData.append(CPTLongTextBox.text!)
          CPTDictionary[CPTCodeData[deleteCounter]] = [shortData[deleteCounter], longData[deleteCounter]]
         deleteCounter+=1
     }
 }
-
